1286877274ab20ffe2f6d1efe93de64fe3e200ac
Tomas Lamuraglia <tomaslamuragliamusic@gmail.com>
2023-09-12 22:48:27 -0300

    pre-entreglable-1

---
+++ js/cursojavascript.js
@@ -0,0 +1,37 @@
+// let nombre = (prompt `Ingrese su nombre`)
+// let producto = ["mouse","teclado","monitor pc","notebook"]
+// let mouse = ["Logitech", "Genius", "Noga"]
+// let teclado = ["Logitech", "Genius", "Noga"]
+// let monitor = ["Samsung", "LG", "Philips", "Dell"]
+// let resultado = 0;
+
+// if (nombre){
+// alert (`bienvenido ${nombre}`)
+// }
+// function buscar (producto, mouse, teclado, monitor, operador){
+//     switch (operador)
+//     case
+//     alert(prompt `que desea buscar?`)}
+
+// buscar(producto)
+// resultado() 
+let nombre= prompt("Ingrese su nombre")
+
+let nombreDelProducto = prompt("Ingrese el nombre del producto")
+
+let cantidadDelProducto = prompt("¿Cuántos artículos desea llevar?")
+if (cantidadDelProducto < 10) {
+    alert("La cantidad es menor que 20");
+    }
+    else if (cantidadDelProducto  < 50) {
+    alert("La cantidad es menor que 50");
+    }
+    else if (cantidadDelProducto  < 100) {
+    alert("La cantidad es menor que 100");
+    }
+    else {
+    alert("La cantidad  es mayor que 100");
+    }
+
+
+alert("Bienvenido" + " " + nombre + " " + " usted ha comprado" + " " +  cantidadDelProducto + " " + nombreDelProducto + " " )
